e50f7a9ba1c6fa60bd94267d847dc05decdd6870
Derek Grech <djgrech@gmail.com>
2025-09-21 18:02:09 +0200

    updated

---
+++ GraphQLSample/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraphQLSample.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TestController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult Get()
+    {
+        return Ok("hello world - gql");
+    }
+}
+
---
+++ kube/remove.bat
@@ -0,0 +1,6 @@
+kubectl delete -f gql\service.yaml
+kubectl delete -f gql\deployment.yaml
+kubectl delete -f sql\service.yaml
+kubectl delete -f sql\deployment.yaml
+kubectl delete -f webapp\service.yaml
+kubectl delete -f webapp\deployment.yaml
